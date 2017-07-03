d65a36abd0af95c06375e7683596a53c9114f448
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-07-03 23:50:03 +0600

    Added routes and alias registering to module bootstrap method

--- BridgeModule.php
+++ BridgeModule.php
@@ -12,5 +12,21 @@ use yii\base\Module;
 
 class BridgeModule extends Module
 {
+    /**
+     * @inheritdoc
+     */
+    public function bootstrap($app)
+    {
+        \Yii::setAlias('@bridge', \Yii::getAlias('@vendor/naffiq/yii2-bridge'));
+        \Yii::setAlias('@bridge-assets', \Yii::getAlias('@vendor/naffiq/yii2-bridge/assets/dist/'));
 
+        if ($app instanceof \yii\web\Application) {
+            $app->getUrlManager()->addRules([
+                ['class' => 'yii\web\UrlRule', 'pattern' => $this->id, 'route' => $this->id . '/default/index'],
+                ['class' => 'yii\web\UrlRule', 'pattern' => $this->id . '/<id:\w+>', 'route' => $this->id . '/default/view'],
+                ['class' => 'yii\web\UrlRule', 'pattern' => $this->id . '/<controller:[\w\-]+>/<action:[\w\-]+>', 'route' => $this->id . '/<controller>/<action>'],
+                ['class' => 'yii\web\UrlRule', 'pattern' => $this->id . '/<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>', 'route' => $this->id . '/<module>/<controller>/<action>'],
+            ], false);
+        }
+    }
 }
