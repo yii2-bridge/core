0162c1203e36c4f5f914f59cf7c685383238c54c
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-07-03 21:31:31 +0600

    added classes to psr-4 autoloader

---
+++ fixtures/SettingsFixture.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: naffiq
+ * Date: 7/3/2017
+ * Time: 9:20 PM
+ */
+
+namespace naffiq\bridge\fixtures;
+
+use yii\test\ActiveFixture;
+
+class SettingsFixture extends ActiveFixture
+{
+    public $modelClass = '\naffiq\bridge\models\Settings';
+}
---
+++ fixtures/UsersFixture.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: naffiq
+ * Date: 7/3/2017
+ * Time: 9:19 PM
+ */
+
+namespace naffiq\bridge\fixtures;
+
+use yii\test\ActiveFixture;
+
+/**
+ * Class UsersFixture
+ * @package naffiq\bridge\fixtures
+ */
+class UsersFixture extends ActiveFixture
+{
+    public $modelClass = '\naffiq\bridge\models\Users';
+}
