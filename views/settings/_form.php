9600c54e318c1fe6691de3b6633cb457c2558f46
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-07-19 00:46:57 +0600

    Using pre-configured TinyMce widget

--- views/settings/_form.php
+++ views/settings/_form.php
@@ -2,8 +2,8 @@
 
 use yii\bootstrap\Html;
 use yii\bootstrap\ActiveForm;
-use dosamigos\tinymce\TinyMce;
-use app\modules\admin\models\Settings;
+use naffiq\bridge\widgets\TinyMce;
+use naffiq\bridge\models\Settings;
 
 /* @var $this yii\web\View */
 /* @var $model Settings */
@@ -18,69 +18,6 @@ use app\modules\admin\models\Settings;
         <?php if ($model->type == Settings::TYPE_TEXT) : ?>
             <?= $form->field($model, 'description')->widget(TinyMce::className(), [
                 'options' => ['rows' => 6],
-                'clientOptions' => [
-                    'plugins' => ['image', 'link'],
-                    'branding' => false,
-                    'images_upload_handler' => new \yii\web\JsExpression(<<<JS
-                function (blobInfo, success, failure) {
-    var xhr, formData;
-
-    xhr = new XMLHttpRequest();
-    xhr.withCredentials = false;
-    xhr.open('POST', '/admin/default/image-upload');
-
-    xhr.onload = function() {
-      var json;
-
-      if (xhr.status != 200) {
-        failure('HTTP Error: ' + xhr.status);
-        return;
-      }
-
-      json = JSON.parse(xhr.responseText);
-
-      if (!json || typeof json.location != 'string') {
-        failure('Invalid JSON: ' + xhr.responseText);
-        return;
-      }
-
-      success(json.location);
-    };
-
-    formData = new FormData();
-    formData.append(yii.getCsrfParam(), yii.getCsrfToken());
-    formData.append('file', blobInfo.blob(), blobInfo.filename());
-
-    xhr.send(formData);
-  }
-JS
-                    ),
-                    'file_picker_types' => 'image',
-                    'file_picker_callback' => new \yii\web\JsExpression(<<<JS
- function(cb, value, meta) {
-            var input = document.createElement('input');
-            input.setAttribute('type', 'file');
-            input.setAttribute('accept', 'image/*');
-            input.onchange = function() {
-                var file = this.files[0];
-
-                var reader = new FileReader();
-                reader.readAsDataURL(file);
-                reader.onload = function () {
-                    var id = 'blobid' + (new Date()).getTime();
-                    var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-                    var base64 = reader.result.split(',')[1];
-                    var blobInfo = blobCache.create(id, file, base64);
-                    blobCache.add(blobInfo);
-                    cb(blobInfo.blobUri(), { title: file.name });
-      };
-            };
-
-            input.click();
-        }
-JS
-                    )
-                ]
             ]) ?>
         <?php elseif ($model->type == Settings::TYPE_IMAGE) : ?>
             <?= Html::img($model->getUploadUrl('value'), ['class' => 'img-thumbnail']) ?>
