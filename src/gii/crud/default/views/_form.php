5fab014df29d9a505b713eb009accc6eb425de9a
naffiq <naffiq@gmail.com>
2018-02-12 07:00:45 +0600

    Fixed CRUD Gii _form

--- src/gii/crud/default/views/_form.php
+++ src/gii/crud/default/views/_form.php
@@ -24,21 +24,24 @@ use naffiq\bridge\widgets\ActiveForm;
 /* @var $form naffiq\bridge\widgets\ActiveForm */
 ?>
 
+<?= "<?php " ?>$form = ActiveForm::begin(); ?>
 <div class="row">
-    <div class="col-lg-5">
-
-        <?= "<?php " ?>$form = ActiveForm::begin(); ?>
+    <div class="col-md-8">
 
 <?php foreach ($generator->getColumnNames() as $attribute) {
     if (in_array($attribute, $safeAttributes) && $attribute !== 'id' && !($generator->generatePositionColumn && $attribute == 'position')) {
         echo ColumnHelper::pushTab(2) .  "<?= " . $generator->generateActiveField($attribute) . " ?>\n\n";
     }
 } ?>
-        <div class="form-group">
-            <?= "<?= " ?>Html::submitButton($model->isNewRecord ? <?= $generator->generateString('Create') ?> : <?= $generator->generateString('Update') ?>, ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        </div>
 
-        <?= "<?php " ?>ActiveForm::end(); ?>
+    </div>
+
+    <div class="col-md-4">
 
     </div>
 </div>
+
+    <div class="form-group">
+        <?= "<?= " ?>Html::submitButton($model->isNewRecord ? <?= $generator->generateString('Create') ?> : <?= $generator->generateString('Update') ?>, ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+<?= "<?php " ?>ActiveForm::end(); ?>
