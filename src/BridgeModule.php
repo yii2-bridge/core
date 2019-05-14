43463a6d38bda52a359c65af4b4c5046d8ecdb50
Altynbek <altynbek.kazezov.97@gmail.com>
2019-05-14 13:47:21 +0500

    Update BridgeModule.php

--- src/BridgeModule.php
+++ src/BridgeModule.php
@@ -93,7 +93,7 @@ class BridgeModule extends Module implements BootstrapInterface
     /**
      * @var string Module version
      */
-    public $version = 'v0.2.5';
+    public $version = 'v0.3.0';
 
     /**
      * @var string Module repository API URL, used to fetch latest version
